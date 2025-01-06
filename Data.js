aad035072b019fb8fe0d6c232a6d2ac1a1e8071f
Kai Xin <22020771@myrp.edu.sg>
2025-01-06 19:29:24 +0800

    L08 App

---
+++ Data.js
@@ -0,0 +1,33 @@
+const datasource = [
+    {
+        data:[
+            {key: 'Kings of the Wyld', ISBN: '9780356509020', copiesOwned: 2, imageUrl: 'https://m.media-amazon.com/images/I/81HsXl+Sp6L._SY522_.jpg'},
+            {key: 'Lies of Locke Lamora', ISBN: '9788420667799', copiesOwned: 1, imageUrl: 'https://m.media-amazon.com/images/I/51uy3iw6GGL._SY445_SX342_.jpg'},
+            {key: 'Harry Potter and the Half-Blood Prince', ISBN: '9781408855706', copiesOwned: 3, imageUrl: 'https://m.media-amazon.com/images/I/81DN1723hUL._SY522_.jpg'}
+    ],
+    title:"Fantasy", bgcolor:"lightgrey"},
+
+    {
+        data:[
+            {key: 'Dune'},
+            {key: 'c'},
+            {key: 'd'},
+    ],
+    title:"Science Fiction (Sci-Fi)", bgcolor:"lightgrey"},
+
+    {
+        data:[
+            {key: 'Kings of the Wyld', ISBN: '9780356509020', imageUrl: 'https://m.media-amazon.com/images/I/81HsXl+Sp6L._SY522_.jpg'},
+            {key: 'Lies of Locke Lamora', }
+        ],
+        title:"Mystery/Thriller", bgcolor:"lightgrey"},
+
+    {
+        data:[
+            {key: 'Kings of the Wyld', ISBN: '9780356509020', imageUrl: 'https://m.media-amazon.com/images/I/81HsXl+Sp6L._SY522_.jpg'},
+            {key: 'Lies of Locke Lamora', }
+        ],
+        title:"Romance", bgcolor:"lightgrey"},
+];
+
+export {datasource};
